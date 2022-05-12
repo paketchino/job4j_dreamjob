2c2e4e941e378604f2964eafcd334ec7c2e7db86
paketchino <chansgorn@gmail.com>
2022-05-12 23:40:24 +0300

    Initial commit

---
+++ src/test/java/ru/job4j/dreamjob/conroller/IndexControlTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.dreamjob.conroller;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class IndexControlTest {
+
+    @Test
+    public void as() {
+        int x = 1;
+        int y = 2;
+        int c = x + y;
+        assertThat(c, is(3));
+    }
+
+}
