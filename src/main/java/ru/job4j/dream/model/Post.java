95f4d8218653c2f056b524ded0107ac9aef42560
paketchino <chansgorn@gmail.com>
2022-05-16 22:19:49 +0300

    7. HTML form. Создание вакансии. [#504844 #301414] fix

--- src/main/java/ru/job4j/dream/model/Post.java
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -56,8 +56,12 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Post post = (Post) o;
         return id == post.id && Objects.equals(name, post.name) && Objects.equals(desc, post.desc) && Objects.equals(created, post.created);
     }
